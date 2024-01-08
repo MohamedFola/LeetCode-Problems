e81c6e1344084b5703731b1f02e71453f3611ce8
MohamedFola <99367479+MohamedFola@users.noreply.github.com>
2024-01-08 14:39:49 +0200

    adding solution of problem 67-add_binary

--- 67_Add_Binary/code.cpp
+++ 67_Add_Binary/code.cpp
@@ -5,7 +5,43 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
+        int f = 0, s = 0, carry = 0;
+        string result = "";
+
+        while( !a.empty() || !b.empty() )
+        {
+            if(!a.empty())
+            {
+                f = a[a.size() - 1] - '0';
+                a.pop_back();
+            }
+
+            if(!b.empty())
+            {
+                s = b[b.size() - 1] - '0';
+                b.pop_back();
+            }
+
+            if ( f + s + carry == 2 )
+            {
+                result += "0";
+                carry = 1;
+            }
+            else if ( f + s + carry == 3 )
+            {
+                result += "1";
+                carry = 1;
+            }
+            else
+            {
+                result += to_string(f + s + carry);
+                carry = 0;
+            }
+
+            f = s = 0;
+        }
+
+        return result;
     }
 };
 
