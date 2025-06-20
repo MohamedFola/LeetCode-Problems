afc5d117fa0dbd15b36295ef88ba2f298a84f1c6
MohamedFola <mohamedhassanfola@gmail.com>
2025-06-20 15:50:52 +0300

    remove .exe files and modify sprial matrix problem

--- 54_Spiral_Matrix/code.cpp
+++ 54_Spiral_Matrix/code.cpp
@@ -12,7 +12,7 @@ public:
         int top = 0;
         int bottom = matrix.size() - 1;
 
-        while(left <= right && top <= bottom)
+        while (left <= right && top <= bottom)
         {
             // go from left to right
             for (int i = left; i <= right ; i++)
@@ -21,12 +21,16 @@ public:
             }
             top++;
 
+            if (top > bottom) break;
+
             // go from top to bottom
             for (int i = top; i <= bottom; i++)
             {
                 result.push_back(matrix[i][right]);
             }
             right--;
+
+            if (left > right) break;
 
             // go from right to left
             for (int i = right; i >= left; i--)
@@ -47,22 +51,105 @@ public:
     }
 };
 
+
 int main()
 {
     Solution s;
 
+    /**********************************************************************/
+    // testing matrix nxn
     std::vector<std::vector<int>> input = {
         {1,2,3},
         {4,5,6},
         {7,8,9}
     };
-
+    
     std::vector<int> result = s.spiralOrder(input);
+    
+    for(auto const& i:result)
+    std::cout << i << "  ";
+    
+    std::cout << std::endl;
+
+    // expected output -> 1 2 3 6 9 8 7 4 5
+    
+    
+    /**********************************************************************/
+    // testing matrix nxm where m > n
+    input = {
+        {1, 2 ,3},
+        {4, 5, 6},
+    };
+
+
+    result = s.spiralOrder(input);
 
     for(auto const& i:result)
         std::cout << i << "  ";
 
     std::cout << std::endl;
+
+    // expected output -> 1 2 3 6 5 4
+
+    
+    /**********************************************************************/
+    // testing matrix nxm where n > m
+    input = {
+        {1, 2},
+        {3, 4},
+        {5, 6},
+        {7, 8},
+        {9, 10},
+        {11, 12},
+    };
+
+    result = s.spiralOrder(input);
+
+    for(auto const& i:result)
+        std::cout << i << "  ";
+
+    std::cout << std::endl;
+
+    // expected output -> 1 2 4 6 8 10 12 11 9 7 5 3
+
+    
+    /**********************************************************************/
+    // testing matrix 1xm
+    input = {
+        {1, 2, 3, 4},
+    };
+
+
+    result = s.spiralOrder(input);
+
+    for(auto const& i:result)
+        std::cout << i << "  ";
+
+    std::cout << std::endl;
+
+    // expected output -> 1 2 3 4
+
+    
+    /**********************************************************************/
+    // testing matrix nx1
+    input = {
+        {1},
+        {2},
+        {3},
+        {4},
+        {5},
+    };
+
+
+    result = s.spiralOrder(input);
+
+    for(auto const& i:result)
+        std::cout << i << "  ";
+
+    std::cout << std::endl;
+
+    // expected output -> 1 2 3 4 5
+
 
     return 0;
 }
